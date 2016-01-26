c6e8a334e50e7c414169de1062497282718ceefd
Marko Tasic <mtasic85@gmail.com>
2016-01-26 17:32:20 +0100

    req-res

---
+++ nodeN.py
@@ -0,0 +1,26 @@
+import json
+import asyncio
+
+from routingtable import Node, Contact
+
+# event loop
+loop = asyncio.get_event_loop()
+
+with open('nodeN.json', 'r') as f:
+    node_config = json.load(f)
+
+for i in range(100):
+    node = Node(
+        loop,
+        id = node_config['id'],
+        listen_host = node_config['listen_host'],
+        listen_port = node_config['listen_port'] + i,
+    )
+
+    for cd in node_config['contacts']:
+        c = Contact(**cd)
+        node.rt.add(c)
+
+# run loop
+loop.run_forever()
+loop.close()
